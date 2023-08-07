8406d97292d3f621fd3485c995565b4cb3999f90
Vijay Neethipudi <60729438+vijayneethipudi@users.noreply.github.com>
2023-08-07 20:40:20 +0530

    providers modified

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,12 @@ terraform {
     backend "gcs" {
         bucket = "dev-us-west2-terraform-backend"
         prefix = "state"
-        credentials = "./creds.json"
+        credentials = file("./creds.json")
     }
 }
 
 provider "google" {
+    credentials = file("./creds.json")
     project = var.gcp_project_id
     region = var.region
 }
